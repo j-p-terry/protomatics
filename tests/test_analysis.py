92c0fafceca06f6cb29dbf23e3de1a071981ed15
j-p-terry <jpterry@uga.edu>
2023-09-02 11:43:41 -0400

    misc updates

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -7,8 +7,13 @@ from astropy.io import fits
 # Add the parent directory to the Python path
 sys.path.insert(0, dirname(dirname(abspath(__file__))))
 
-from protomatics.analysis import calc_azimuthal_average, make_grids, make_peak_vel_map
-from protomatics.moments import make_moments
+from protomatics.analysis import (
+    calc_azimuthal_average,
+    get_wiggle_amplitude,
+    make_grids,
+    make_peak_vel_map,
+)
+from protomatics.moments import calculate_keplerian_moment1, extract_wiggle, make_moments
 
 
 @pytest.mark.parametrize("fits_name", ["test_3d_cube.fits", "test_6d_cube.fits"])
@@ -71,3 +76,45 @@ def test_peak_vel_map(fits_name):
     _ = make_peak_vel_map(path)
 
     print("Passed!")
+
+
+@pytest.mark.parametrize("fits_name", ["test_3d_cube.fits", "test_6d_cube.fits"])
+def test_wiggle_amplitude(fits_name):
+    """Tests functions regarding extracting and analyzing wiggle"""
+
+    print(f"Testing wiggle ampltidue with {fits_name}")
+
+    # plot with preloaded
+    path = f"./tests/data/{fits_name}"
+
+    print("Calculating moments")
+    calc_moments, _ = make_moments(path, which_moments=[1])
+
+    print("Extracting wiggle")
+    rs, phis = extract_wiggle(calc_moments[1], in_pv_space=False)
+
+    print("Getting ampltidue without reference curve")
+    _ = get_wiggle_amplitude(rs, phis, vel_is_zero=True)
+
+    print("Getting standard without reference curve")
+    _ = get_wiggle_amplitude(rs, phis, vel_is_zero=True, use_std_as_amp=True)
+
+    print("Getting ampltidue with identical reference curve")
+    _ = get_wiggle_amplitude(rs, phis, ref_rs=rs, ref_phis=phis, vel_is_zero=False)
+
+    print("Getting standard with identical reference curve")
+    _ = get_wiggle_amplitude(
+        rs, phis, ref_rs=rs, ref_phis=phis, vel_is_zero=False, use_std_as_amp=True
+    )
+
+    kep_moment1 = calculate_keplerian_moment1(hdu=fits.open(path))
+
+    kep_rs, kep_phis = extract_wiggle(kep_moment1, in_pv_space=False)
+
+    print("Getting ampltidue with keplerian reference curve")
+    _ = get_wiggle_amplitude(rs, phis, ref_rs=kep_rs, ref_phis=kep_phis, vel_is_zero=False)
+
+    print("Getting standard with keplerian reference curve")
+    _ = get_wiggle_amplitude(
+        rs, phis, ref_rs=kep_rs, ref_phis=kep_phis, vel_is_zero=False, use_std_as_amp=True
+    )
