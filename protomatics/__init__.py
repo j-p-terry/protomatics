21b796b67fb449af5a5e2f3caf504385af999331
j-p-terry <jpterry@uga.edu>
2023-10-27 13:15:57 -0400

    0.4.5

--- protomatics/__init__.py
+++ protomatics/__init__.py
@@ -4,4 +4,4 @@ from .helpers import *
 from .moments import *
 from .plotting import *
 
-__version__ = "0.4.4"
+__version__ = "0.4.5"
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(this_directory, "README.md")) as f:
 
 setuptools.setup(
     name="protomatics",
-    version="0.4.4",
+    version="0.4.5",
     author="Jason Terry",
     author_email="jpterry@uga.edu",
     packages=["protomatics"],
