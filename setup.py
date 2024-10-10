1031af5d640fe52dc16bf56c90a23391ecc71c7c
j-p-terry <jpterry@uga.edu>
2024-10-10 14:24:30 +0100

    0.7.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(this_directory, "README.md")) as f:
 
 setuptools.setup(
     name="protomatics",
-    version="0.7.3",
+    version="0.7.5",
     author="Jason Terry",
     author_email="jpterry@uga.edu",
     packages=["protomatics"],
